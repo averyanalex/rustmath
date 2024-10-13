c7e21d39e6e74650b347ccdea082e09d9e6b828c
AveryanAlex <alex@averyan.ru>
2024-10-13 15:44:13 +0300

    Rayon + mul

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 use std::{
     fmt::{self, Debug, Display},
-    io::BufRead,
-    ops::{Div, Mul, Sub},
-    str::FromStr,
+    io::stdin,
+    ops::Mul,
 };
 
-use num::{BigRational, Zero};
+use num::{BigRational, FromPrimitive, Zero};
+use rayon::prelude::*;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Matrix<T>(Vec<Vec<T>>);
+
+impl<const I: usize, const J: usize> From<[[i32; J]; I]> for Matrix<BigRational> {
+    fn from(value: [[i32; J]; I]) -> Self {
+        Self(
+            value
+                .into_iter()
+                .map(|row| {
+                    row.into_iter()
+                        .map(|n| BigRational::from_i32(n).unwrap())
+                        .collect()
+                })
+                .collect(),
+        )
+    }
+}
 
 impl<T: Display> Display for Matrix<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -26,23 +41,34 @@ impl<T: Display> Display for Matrix<T> {
     }
 }
 
-fn read_nums<T>(io: &mut impl BufRead) -> Vec<T>
-where
-    T: FromStr,
-    <T as FromStr>::Err: Debug,
-{
-    let mut buf = String::new();
-    io.read_line(&mut buf).unwrap();
+impl Mul for Matrix<BigRational> {
+    type Output = Self;
 
-    buf.split_whitespace().map(|s| s.parse().unwrap()).collect()
+    fn mul(self, rhs: Self) -> Self::Output {
+        assert!(self.0.len() > 1);
+        assert_eq!(self.0[0].len(), rhs.0.len());
+        let m = rhs.0.len();
+
+        let mat = (0..self.0.len())
+            .map(|i| {
+                (0..rhs.0[0].len())
+                    .map(|j| (0..m).map(|idx| &self.0[i][idx] * &rhs.0[idx][j]).sum())
+                    .collect()
+            })
+            .collect();
+        Self(mat)
+    }
 }
 
 fn main() {
-    let mut stdin = std::io::stdin().lock();
-
-    let mut mat = vec![read_nums::<BigRational>(&mut stdin)];
-    for _ in 1..mat[0].len() - 1 {
-        mat.push(read_nums(&mut stdin));
+    let mut mat = Vec::new();
+    for line in stdin().lines() {
+        mat.push(
+            line.unwrap()
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        );
     }
 
     let mut mat = Matrix(mat);
@@ -53,38 +79,18 @@ fn main() {
     print!("{mat}");
     println!("===========================================================");
 
-    if mat.0.iter().any(|row| {
-        !row.last().unwrap().is_zero() && row[..row.len() - 1].iter().all(|elem| elem.is_zero())
-    }) {
-        println!("No solution");
-    } else {
-        for (row_idx, row) in mat.0.iter().enumerate() {
-            if let Some(non_zero_idx) = row.iter().position(|elem| !elem.is_zero()) {
-                let mut answer = format!("x_{} = {}", row_idx + 1, row.last().unwrap());
-                for (idx, elem) in row.iter().enumerate().skip(non_zero_idx + 1) {
-                    if idx != row.len() - 1 && !elem.is_zero() {
-                        answer += &format!(" - {} * x_{}", elem, idx + 1);
-                    }
-                }
-                answer += &format!(" , i.e. {}", row.last().unwrap());
-                println!("{}", answer);
-            } else {
-                println!("x_{} is free, i.e. 0", row_idx + 1);
-            }
-        }
-    }
+    // let a = Matrix::from([[1, 0, 0], [0, 1, 0], [0, 0, 1], [8, -1, -6]]);
+    // let b = Matrix::from([[1, 0, 2, 2], [0, 1, 6, 5], [3, 3, 24, 21]]);
+    // let c1 = Matrix::from([
+    //     [-11, 6, 1, -1],
+    //     [-10, 5, 1, -1],
+    //     [-2, 2, 1, 0],
+    //     [10, -6, -2, 1],
+    // ]);
+    // print!("{}", a * b * c1);
 }
 
-fn gauss<T>(mat: &mut Matrix<T>)
-where
-    T: Clone,
-    T: PartialEq<T>,
-    T: Zero,
-    T: Div<Output = T>,
-    T: Mul<Output = T>,
-    T: Sub<Output = T>,
-    T: Debug,
-{
+fn gauss(mat: &mut Matrix<BigRational>) {
     let columns = mat.0[0].len();
     let rows = mat.0.len();
     for row in mat.0.iter().skip(1) {
@@ -100,7 +106,7 @@ where
                 .enumerate()
                 .skip(cur_row_idx)
                 .find_map(|(row_idx, row)| {
-                    if row[col] != T::zero() {
+                    if !row[col].is_zero() {
                         Some(row_idx)
                     } else {
                         None
@@ -110,20 +116,23 @@ where
             mat.0.swap(non_zero_row, cur_row_idx);
 
             let factor = mat.0[cur_row_idx][col].clone();
-            for col in mat.0[cur_row_idx].iter_mut() {
-                *col = col.clone() / factor.clone();
-            }
+            mat.0[cur_row_idx].par_iter_mut().for_each(|col| {
+                *col /= &factor;
+            });
 
-            for row_idx in 0..rows {
-                if row_idx != cur_row_idx {
-                    let factor = mat.0[row_idx][col].clone();
+            let cur_row = mat.0[cur_row_idx].clone();
+            mat.0
+                .par_iter_mut()
+                .enumerate()
+                .filter(|(row_idx, _)| *row_idx != cur_row_idx)
+                .map(|(_, row)| row)
+                .for_each(|row| {
+                    let factor = row[col].clone();
 
-                    for col_idx in 0..columns {
-                        mat.0[row_idx][col_idx] = mat.0[row_idx][col_idx].clone()
-                            - mat.0[cur_row_idx][col_idx].clone() * factor.clone();
+                    for (col, cur_col) in row.iter_mut().zip(cur_row.iter()) {
+                        *col -= cur_col * &factor;
                     }
-                }
-            }
+                });
 
             cur_row_idx += 1;
         }
@@ -132,33 +141,46 @@ where
 
 #[cfg(test)]
 mod tests {
-    use num::FromPrimitive;
-
     use super::*;
-
-    fn to_rational_mat(mat: Vec<Vec<i64>>) -> Matrix<BigRational> {
-        Matrix(
-            mat.into_iter()
-                .map(|row| {
-                    row.into_iter()
-                        .map(|n| BigRational::from_i64(n).unwrap())
-                        .collect()
-                })
-                .collect(),
-        )
-    }
+    use num::FromPrimitive;
 
     #[test]
     fn simple_gauss() {
-        let mut mat = to_rational_mat(vec![
-            vec![3, 2, -5, -1],
-            vec![2, -1, 3, 13],
-            vec![1, 2, -1, 9],
-        ]);
+        let mut mat = Matrix::from([[3, 2, -5, -1], [2, -1, 3, 13], [1, 2, -1, 9]]);
         gauss(&mut mat);
         assert_eq!(
             mat,
-            to_rational_mat(vec![vec![1, 0, 0, 3], vec![0, 1, 0, 5], vec![0, 0, 1, 4]]),
+            Matrix::from([[1, 0, 0, 3], [0, 1, 0, 5], [0, 0, 1, 4]]),
         );
+    }
+
+    #[test]
+    fn big_gauss() {
+        let mut mat = Matrix(
+            (0..100)
+                .map(|i| {
+                    (0..100)
+                        .map(|j| i * 100 + j)
+                        .map(|x| BigRational::from_i32(x).unwrap())
+                        .collect()
+                })
+                .collect(),
+        );
+
+        gauss(&mut mat);
+    }
+
+    #[test]
+    fn test_mul() {
+        let a = Matrix::from([[1, 0, 0], [0, 1, 0], [0, 0, 1], [8, -1, -6]]);
+        let b = Matrix::from([[1, 0, 2, 2], [0, 1, 6, 5], [3, 3, 24, 21]]);
+        let c = Matrix::from([
+            [1, 0, 2, 2],
+            [0, 1, 6, 5],
+            [3, 3, 24, 21],
+            [-10, -19, -134, -115],
+        ]);
+
+        assert_eq!(a * b, c);
     }
 }
